ec16737d84ee2519e281361c76706fb41246c23d
Akhi <114737969+Akhila143L@users.noreply.github.com>
2022-10-11 09:50:51 +0530

    Add files via upload

---
+++ DemoBean.java
@@ -0,0 +1,15 @@
+package com.infinite.bean;
+
+public class DemoBean {
+	
+private String greeting = "Good Morning...";
+
+public String getGreeting() {
+	return greeting;
+}
+
+public void setGreeting(String greeting) {
+	this.greeting = greeting;
+}
+
+}
